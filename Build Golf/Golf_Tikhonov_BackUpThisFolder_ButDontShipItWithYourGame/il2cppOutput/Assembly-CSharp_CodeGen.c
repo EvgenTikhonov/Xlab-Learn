2f3819af94ec4b6368f3dbc9d4ebaa5734704a7e
Evgenii Tikhonov <e.tikhonov@carx-tech.com>
2025-04-18 18:24:53 +0300

    FINAL

---
+++ Build Golf/Golf_Tikhonov_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,281 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Generic::Test(T&)
+// 0x00000002 System.Void Generic::Start()
+extern void Generic_Start_mF84933C67E501713D2EF226A9EEAFACCB68A6F00 (void);
+// 0x00000003 System.Void Generic::.ctor()
+extern void Generic__ctor_mFAEE49787B56618D31C5D4DFF1D29A35B3A34483 (void);
+// 0x00000004 System.Void Generic/MyClass::.ctor()
+extern void MyClass__ctor_m571207202B5A38DE489BDBEB013E7B1F79442DFE (void);
+// 0x00000005 System.Int32 Generic/MyList`1::get_count()
+// 0x00000006 System.Void Generic/MyList`1::Push(Titem)
+// 0x00000007 System.Void Generic/MyList`1::Insert(System.Int32,Titem)
+// 0x00000008 System.Void Generic/MyList`1::Remove(System.Int32)
+// 0x00000009 System.Void Generic/MyList`1::Clear()
+// 0x0000000A System.Void Generic/MyList`1::IndexOf(Titem)
+// 0x0000000B System.Void Generic/MyList`1::RemoveAt(Titem)
+// 0x0000000C System.Void Generic/MyList`1::.ctor()
+// 0x0000000D System.Void FlyCameraBehaviour::Start()
+extern void FlyCameraBehaviour_Start_m8FA571017E3B88279D13680390332F6AFF8D8C6C (void);
+// 0x0000000E System.Void FlyCameraBehaviour::Update()
+extern void FlyCameraBehaviour_Update_mB5589D46E840CCA791FCBC3C82D393B3C8325121 (void);
+// 0x0000000F System.Void FlyCameraBehaviour::.ctor()
+extern void FlyCameraBehaviour__ctor_m44B57FCC9DD0B6CEBB559F796FF34D1BF956D971 (void);
+// 0x00000010 System.Void Golf.GameController::Start()
+extern void GameController_Start_m4816F4ED9FA22ADA31013664D51E06997B317D8E (void);
+// 0x00000011 System.Void Golf.GameController::.ctor()
+extern void GameController__ctor_m6A1335A3584F395AF2AC9DB6FA73BE57588B2B35 (void);
+// 0x00000012 System.Void Golf.GameEvents::CollisionStonesInvoke(UnityEngine.Collision)
+extern void GameEvents_CollisionStonesInvoke_m0EB21DC9D82E57D2E01180B6C612991BDB0B4757 (void);
+// 0x00000013 System.Void Golf.GameEvents::StickHit()
+extern void GameEvents_StickHit_m967B30476C07F61BDA9E85D7D40EBC1ADFE72504 (void);
+// 0x00000014 System.Void Golf.GameEvents::.ctor()
+extern void GameEvents__ctor_mF65F6E587C4DD7F2D1C4F13ED4527B020D6DDC16 (void);
+// 0x00000015 System.Void Golf.LevelController::Start()
+extern void LevelController_Start_mFE8469F6BAFAA3C52FA119A2C14FF2ADC9A18E3A (void);
+// 0x00000016 System.Void Golf.LevelController::OnStickHit()
+extern void LevelController_OnStickHit_m62AE00831C1DADBD0E48599F433FE68F49459291 (void);
+// 0x00000017 System.Void Golf.LevelController::OnEnable()
+extern void LevelController_OnEnable_m082DB797E40E998BA85FE6196451E9C2BB19D049 (void);
+// 0x00000018 System.Void Golf.LevelController::OnDisable()
+extern void LevelController_OnDisable_mDA169F0A9DF22F2A5331909DA2A871F76F41B4D8 (void);
+// 0x00000019 System.Void Golf.LevelController::GameOver()
+extern void LevelController_GameOver_m8673CC5929795ED3B360C252E1F65B34CABF48BE (void);
+// 0x0000001A System.Void Golf.LevelController::ClearStones()
+extern void LevelController_ClearStones_mDE99F38B9E83B8DC4F8C6B3C1147C86C71233FE4 (void);
+// 0x0000001B System.Void Golf.LevelController::RefreshDelay()
+extern void LevelController_RefreshDelay_mBFDA20A98CEB21332CBE6F0DF5B87DD0E6F81C5E (void);
+// 0x0000001C System.Void Golf.LevelController::Update()
+extern void LevelController_Update_m2D79F0E466C6843FB5785252E9C59BFC7DE9A452 (void);
+// 0x0000001D System.Void Golf.LevelController::.ctor()
+extern void LevelController__ctor_m1EC236799CF01043DEB3E19760768EFB76D020D1 (void);
+// 0x0000001E System.Void Golf.Player::Update()
+extern void Player_Update_mB677F6409677997FD84A3155EE62F3EEDFEC278E (void);
+// 0x0000001F System.Void Golf.Player::SetDown(System.Boolean)
+extern void Player_SetDown_mA4A6B1158863B6A514F87138AE1B8814CE216CB4 (void);
+// 0x00000020 System.Void Golf.Player::OnCollisionStick(UnityEngine.Collider)
+extern void Player_OnCollisionStick_m2A520AB2BA8CF9A81AF03113A65D618BDA28BECE (void);
+// 0x00000021 System.Void Golf.Player::.ctor()
+extern void Player__ctor_m86480D2FF1568D6A54D42513D689FC393E94E981 (void);
+// 0x00000022 System.Void Golf.PlayerController::Update()
+extern void PlayerController_Update_mBF4BC83B926399E611837EEACC5433F2CCB849FB (void);
+// 0x00000023 System.Void Golf.PlayerController::OnDown()
+extern void PlayerController_OnDown_mC6A5136089286116A0C45EED98C06272F8724871 (void);
+// 0x00000024 System.Void Golf.PlayerController::OnUp()
+extern void PlayerController_OnUp_mC2B0AAA38D42B0951627D5062F563D0A48F5A78F (void);
+// 0x00000025 System.Void Golf.PlayerController::Start()
+extern void PlayerController_Start_m0EB1B56F51A7026B11C22C5E4CC656ADB1EC59D9 (void);
+// 0x00000026 System.Void Golf.PlayerController::.ctor()
+extern void PlayerController__ctor_m2E1E8C4C256C139BE7953A7A8133A62D144EF0D7 (void);
+// 0x00000027 UnityEngine.GameObject Golf.SpawnerStone::Spawn()
+extern void SpawnerStone_Spawn_mD867C8F3D0F73966E5A81CB286B4C4D781AE4A2F (void);
+// 0x00000028 UnityEngine.GameObject Golf.SpawnerStone::GetRandomPrefab()
+extern void SpawnerStone_GetRandomPrefab_mEBC5E5A94125FC2450556CB44B644CA7573C300F (void);
+// 0x00000029 System.Void Golf.SpawnerStone::.ctor()
+extern void SpawnerStone__ctor_m8DF7A492258CDBBCE554940FF721F95426E52EA5 (void);
+// 0x0000002A System.Void Golf.GameoverState::Restart()
+extern void GameoverState_Restart_m3D948020A6638EB2594BF16BFCB39E95D48D497B (void);
+// 0x0000002B System.Void Golf.GameoverState::.ctor()
+extern void GameoverState__ctor_m1E3BC5723D5340A3B59E8C18ECB113AF6DEBB275 (void);
+// 0x0000002C System.Void Golf.GameplayState::OnEnable()
+extern void GameplayState_OnEnable_mFE88CEA9E21E80D950E283D5CE1C55F430DC203B (void);
+// 0x0000002D System.Void Golf.GameplayState::OnStickHit()
+extern void GameplayState_OnStickHit_mF68CFC2956023E26B8A9954DFBEE07A6A15F7FCF (void);
+// 0x0000002E System.Void Golf.GameplayState::OnGameOver()
+extern void GameplayState_OnGameOver_mBE1D89D358F39E018AD8F17E3AD437E30E109D55 (void);
+// 0x0000002F System.Void Golf.GameplayState::OnDisable()
+extern void GameplayState_OnDisable_m8D4764DA54A112DA1BBED9192D1A038149CD3A93 (void);
+// 0x00000030 System.Void Golf.GameplayState::.ctor()
+extern void GameplayState__ctor_m3988A63EF68B104A2AD360F91B5B74978BDA9351 (void);
+// 0x00000031 System.Void Golf.GameState::Enter()
+extern void GameState_Enter_m868423106ED3C6A700D643C06F83C9D96CC96039 (void);
+// 0x00000032 System.Void Golf.GameState::Exit()
+extern void GameState_Exit_mE435BEE36E6D2CAB6518BB98C00E196336107AEA (void);
+// 0x00000033 System.Void Golf.GameState::OnEnable()
+extern void GameState_OnEnable_mA4BFFC22BC4375345AF81FF3D22DC9F23502D6F3 (void);
+// 0x00000034 System.Void Golf.GameState::OnDisable()
+extern void GameState_OnDisable_m15BA97AF2E80297623C498E9D2053FF16A1AE2BC (void);
+// 0x00000035 System.Void Golf.GameState::.ctor()
+extern void GameState__ctor_m0A6C4B761FF09435447CCAAF7113055E7070BCED (void);
+// 0x00000036 System.Void Golf.MainMenuState::PlayGame()
+extern void MainMenuState_PlayGame_mD491374A3DB1C2564B9138A4B472EEEA5E8AA343 (void);
+// 0x00000037 System.Void Golf.MainMenuState::OnEnable()
+extern void MainMenuState_OnEnable_mD4610FA06BFE9F85E7FE0DB8E35316A6AFCECF68 (void);
+// 0x00000038 System.Void Golf.MainMenuState::.ctor()
+extern void MainMenuState__ctor_mCD813C657FDE2E7B30B2D36F3B502D0A01061354 (void);
+// 0x00000039 System.Void Golf.Stick::OnCollisionEnter(UnityEngine.Collision)
+extern void Stick_OnCollisionEnter_mCF833031225547F08ECC7DB83EEC0A12BEE54345 (void);
+// 0x0000003A System.Void Golf.Stick::.ctor()
+extern void Stick__ctor_mCA801BB43B895AFAF5683000A038C061F54FBE7B (void);
+// 0x0000003B System.Void Golf.Stone::OnCollisionEnter(UnityEngine.Collision)
+extern void Stone_OnCollisionEnter_mE8B1A0BB020E442B77889AC6163F86FAECE740EB (void);
+// 0x0000003C System.Void Golf.Stone::.ctor()
+extern void Stone__ctor_m52DF9AA5D4491A29A6A53D6D9105B4D86EEB8E95 (void);
+static Il2CppMethodPointer s_methodPointers[60] = 
+{
+	NULL,
+	Generic_Start_mF84933C67E501713D2EF226A9EEAFACCB68A6F00,
+	Generic__ctor_mFAEE49787B56618D31C5D4DFF1D29A35B3A34483,
+	MyClass__ctor_m571207202B5A38DE489BDBEB013E7B1F79442DFE,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	FlyCameraBehaviour_Start_m8FA571017E3B88279D13680390332F6AFF8D8C6C,
+	FlyCameraBehaviour_Update_mB5589D46E840CCA791FCBC3C82D393B3C8325121,
+	FlyCameraBehaviour__ctor_m44B57FCC9DD0B6CEBB559F796FF34D1BF956D971,
+	GameController_Start_m4816F4ED9FA22ADA31013664D51E06997B317D8E,
+	GameController__ctor_m6A1335A3584F395AF2AC9DB6FA73BE57588B2B35,
+	GameEvents_CollisionStonesInvoke_m0EB21DC9D82E57D2E01180B6C612991BDB0B4757,
+	GameEvents_StickHit_m967B30476C07F61BDA9E85D7D40EBC1ADFE72504,
+	GameEvents__ctor_mF65F6E587C4DD7F2D1C4F13ED4527B020D6DDC16,
+	LevelController_Start_mFE8469F6BAFAA3C52FA119A2C14FF2ADC9A18E3A,
+	LevelController_OnStickHit_m62AE00831C1DADBD0E48599F433FE68F49459291,
+	LevelController_OnEnable_m082DB797E40E998BA85FE6196451E9C2BB19D049,
+	LevelController_OnDisable_mDA169F0A9DF22F2A5331909DA2A871F76F41B4D8,
+	LevelController_GameOver_m8673CC5929795ED3B360C252E1F65B34CABF48BE,
+	LevelController_ClearStones_mDE99F38B9E83B8DC4F8C6B3C1147C86C71233FE4,
+	LevelController_RefreshDelay_mBFDA20A98CEB21332CBE6F0DF5B87DD0E6F81C5E,
+	LevelController_Update_m2D79F0E466C6843FB5785252E9C59BFC7DE9A452,
+	LevelController__ctor_m1EC236799CF01043DEB3E19760768EFB76D020D1,
+	Player_Update_mB677F6409677997FD84A3155EE62F3EEDFEC278E,
+	Player_SetDown_mA4A6B1158863B6A514F87138AE1B8814CE216CB4,
+	Player_OnCollisionStick_m2A520AB2BA8CF9A81AF03113A65D618BDA28BECE,
+	Player__ctor_m86480D2FF1568D6A54D42513D689FC393E94E981,
+	PlayerController_Update_mBF4BC83B926399E611837EEACC5433F2CCB849FB,
+	PlayerController_OnDown_mC6A5136089286116A0C45EED98C06272F8724871,
+	PlayerController_OnUp_mC2B0AAA38D42B0951627D5062F563D0A48F5A78F,
+	PlayerController_Start_m0EB1B56F51A7026B11C22C5E4CC656ADB1EC59D9,
+	PlayerController__ctor_m2E1E8C4C256C139BE7953A7A8133A62D144EF0D7,
+	SpawnerStone_Spawn_mD867C8F3D0F73966E5A81CB286B4C4D781AE4A2F,
+	SpawnerStone_GetRandomPrefab_mEBC5E5A94125FC2450556CB44B644CA7573C300F,
+	SpawnerStone__ctor_m8DF7A492258CDBBCE554940FF721F95426E52EA5,
+	GameoverState_Restart_m3D948020A6638EB2594BF16BFCB39E95D48D497B,
+	GameoverState__ctor_m1E3BC5723D5340A3B59E8C18ECB113AF6DEBB275,
+	GameplayState_OnEnable_mFE88CEA9E21E80D950E283D5CE1C55F430DC203B,
+	GameplayState_OnStickHit_mF68CFC2956023E26B8A9954DFBEE07A6A15F7FCF,
+	GameplayState_OnGameOver_mBE1D89D358F39E018AD8F17E3AD437E30E109D55,
+	GameplayState_OnDisable_m8D4764DA54A112DA1BBED9192D1A038149CD3A93,
+	GameplayState__ctor_m3988A63EF68B104A2AD360F91B5B74978BDA9351,
+	GameState_Enter_m868423106ED3C6A700D643C06F83C9D96CC96039,
+	GameState_Exit_mE435BEE36E6D2CAB6518BB98C00E196336107AEA,
+	GameState_OnEnable_mA4BFFC22BC4375345AF81FF3D22DC9F23502D6F3,
+	GameState_OnDisable_m15BA97AF2E80297623C498E9D2053FF16A1AE2BC,
+	GameState__ctor_m0A6C4B761FF09435447CCAAF7113055E7070BCED,
+	MainMenuState_PlayGame_mD491374A3DB1C2564B9138A4B472EEEA5E8AA343,
+	MainMenuState_OnEnable_mD4610FA06BFE9F85E7FE0DB8E35316A6AFCECF68,
+	MainMenuState__ctor_mCD813C657FDE2E7B30B2D36F3B502D0A01061354,
+	Stick_OnCollisionEnter_mCF833031225547F08ECC7DB83EEC0A12BEE54345,
+	Stick__ctor_mCA801BB43B895AFAF5683000A038C061F54FBE7B,
+	Stone_OnCollisionEnter_mE8B1A0BB020E442B77889AC6163F86FAECE740EB,
+	Stone__ctor_m52DF9AA5D4491A29A6A53D6D9105B4D86EEB8E95,
+};
+static const int32_t s_InvokerIndices[60] = 
+{
+	0,
+	5503,
+	5503,
+	5503,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	9436,
+	10245,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	4307,
+	4394,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5384,
+	5384,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	5503,
+	4394,
+	5503,
+	4394,
+	5503,
+};
+static const Il2CppTokenRangePair s_rgctxIndices[1] = 
+{
+	{ 0x02000005, { 0, 1 } },
+};
+extern const uint32_t g_rgctx_TitemU5BU5D_tBC5065B4931D623AD625C489A4BEC6C7A28A3A55;
+static const Il2CppRGCTXDefinition s_rgctxValues[1] = 
+{
+	{ (Il2CppRGCTXDataType)2, (const void *)&g_rgctx_TitemU5BU5D_tBC5065B4931D623AD625C489A4BEC6C7A28A3A55 },
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	60,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	1,
+	s_rgctxIndices,
+	1,
+	s_rgctxValues,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
